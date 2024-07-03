be48aeb478777f6d1410e0810f5d5b57b351c1a3
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-07-04 02:05:42 +0530

    arrange the syntax

---
+++ .history/app/contact/page_20240704020325.js
@@ -0,0 +1,11 @@
+import Contact from '@/components/Contact'
+
+export const metadata={
+    title:"Contact Us Page",
+    description:'Contact Form'
+}
+
+export default function Page() {
+    return(
+        <Contact/>
+    )
---
+++ .history/app/contact/page_20240704020330.js
@@ -0,0 +1,12 @@
+import Contact from '@/components/Contact'
+
+export const metadata={
+    title:"Contact Us Page",
+    description:'Contact Form'
+}
+
+export default function Page() {
+    return(
+        <Contact/>
+    )
+}
